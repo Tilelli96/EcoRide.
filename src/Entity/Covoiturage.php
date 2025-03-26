c4f90d36fa0ffff21ae6ae3ba008bfa2bd35b7cd
haidou tounsia <haidou.tounsia@gmail.com>
2025-03-26 12:45:37 +0100

    création de l'entité Covoiturage

---
+++ src/Entity/Covoiturage.php
@@ -0,0 +1,179 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CovoiturageRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use App\Entity\User;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
+
+#[ORM\Entity(repositoryClass: CovoiturageRepository::class)]
+class Covoiturage
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    #[Assert\GreaterThanOrEqual('today')]
+    private ?\DateTimeInterface $date_depart = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $date_arrivee = null;
+
+    #[ORM\Column(type: Types::TIME_MUTABLE)]
+    private ?\DateTimeInterface $heure_depart = null;
+
+    #[ORM\Column(type: Types::TIME_MUTABLE)]
+    private ?\DateTimeInterface $heure_arrivee = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $lieu_depart = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $lieu_arrivee = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $statut = null;
+
+    #[ORM\Column]
+    #[Assert\GreaterThan(0)]
+    #[Assert\LessThan(5)]
+    private ?int $nb_places = null;
+
+    #[ORM\Column]
+    private ?float $prix_personne = null;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Voiture $voiture = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDateDepart(): ?\DateTimeInterface
+    {
+        return $this->date_depart;
+    }
+
+    public function setDateDepart(\DateTimeInterface $date_depart): static
+    {
+        $this->date_depart = $date_depart;
+
+        return $this;
+    }
+
+    public function getDateArrivee(): ?\DateTimeInterface
+    {
+        return $this->date_arrivee;
+    }
+
+    public function setDateArrivee(\DateTimeInterface $date_arrivee): static
+    {
+        $this->date_arrivee = $date_arrivee;
+
+        return $this;
+    }
+
+    public function getHeureDepart(): ?\DateTimeInterface
+    {
+        return $this->heure_depart;
+    }
+
+    public function setHeureDepart(\DateTimeInterface $heure_depart): static
+    {
+        $this->heure_depart = $heure_depart;
+
+        return $this;
+    }
+
+    public function getHeureArrivee(): ?\DateTimeInterface
+    {
+        return $this->heure_arrivee;
+    }
+
+    public function setHeureArrivee(\DateTimeInterface $heure_arrivee): static
+    {
+        $this->heure_arrivee = $heure_arrivee;
+
+        return $this;
+    }
+
+    public function getLieuDepart(): ?string
+    {
+        return $this->lieu_depart;
+    }
+
+    public function setLieuDepart(string $lieu_depart): static
+    {
+        $this->lieu_depart = $lieu_depart;
+
+        return $this;
+    }
+
+    public function getLieuArrivee(): ?string
+    {
+        return $this->lieu_arrivee;
+    }
+
+    public function setLieuArrivee(string $lieu_arrivee): static
+    {
+        $this->lieu_arrivee = $lieu_arrivee;
+
+        return $this;
+    }
+
+    public function getStatut(): ?string
+    {
+        return $this->statut;
+    }
+
+    public function setStatut(string $statut): static
+    {
+        $this->statut = $statut;
+
+        return $this;
+    }
+
+    public function getNbPlaces(): ?int
+    {
+        return $this->nb_places;
+    }
+
+    public function setNbPlaces(int $nb_places): static
+    {
+        $this->nb_places = $nb_places;
+
+        return $this;
+    }
+
+    public function getPrixPersonne(): ?float
+    {
+        return $this->prix_personne;
+    }
+
+    public function setPrixPersonne(float $prix_personne): static
+    {
+        $this->prix_personne = $prix_personne;
+
+        return $this;
+    }
+
+    public function getVoiture(): ?Voiture
+    {
+        return $this->voiture;
+    }
+
+    public function setVoiture(?Voiture $voiture): static
+    {
+        $this->voiture = $voiture;
+
+        return $this;
+    }
+}
