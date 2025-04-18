08e8185ce02e7ea20c46ef8c25a09467c10e04e8
haidou tounsia <haidou.tounsia@gmail.com>
2025-04-18 23:08:56 +0200

    Controller test pour Heroku

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,36 @@
 
 namespace App\Controller;
 
-
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-
+use Symfony\Component\Routing\Attribute\Route;
+use App\Form\SearchType;
+use App\Document\Preferences;
+use App\Form\PreferencesType;
+use Doctrine\ODM\MongoDB\DocumentManager;
 
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(DocumentManager $dm): Response
     {
-        return new Response('<h1>MERDEUH!!!</h1>');
+        $form = $this->createForm(SearchType::class);
+        $user = $this->getUser();
+
+        if($user){
+            $preferences = $dm->getRepository(Preferences::class)->findOneBy(['userId' => $user->getId()]) ?? null;
+        }else{
+            $preferences = null;
+        }
+
+        return $this->render('home/index.html.twig', [
+            'form' => $form,
+            'preferences' => $preferences
+        ]);
+    }
+    #[Route('/mentionsLegales')]
+    public function mentionsLegales(): Response
+    {
+        return $this->render('home/mentionsLegales.html.twig');
     }
 }
