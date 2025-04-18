24cc4c827f57a4be7261a999a300f826181d0512
haidou tounsia <haidou.tounsia@gmail.com>
2025-04-18 22:47:55 +0200

    Controller test pour Heroku

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,7 +11,6 @@ final class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        $html = file_get_contents(__DIR__ . '/../../public/index.html');
-        return new Response($html);
+        return new Response('<h1>MERDEUH</h1>');
     }
 }
