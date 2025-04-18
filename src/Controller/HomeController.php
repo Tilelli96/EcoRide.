645cf0775e1440e9c74498da33bb556a4113bda4
haidou tounsia <haidou.tounsia@gmail.com>
2025-04-18 22:38:16 +0200

    Controller test pour Heroku

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,6 +11,7 @@ final class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
+        $html = file_get_contents(__DIR__ . '/../../public/index.html');
+        return new Response($html);
     }
 }
